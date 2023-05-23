dea55635e92664572ef36ba789203d326fa0304a
mcbauman <m.c.baumann@icloud.com>
2023-05-23 17:40:19 +0200

    first and final

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref } from "vue";
+import BaseInput from "./BaseInput.vue";
+const response=[
+  {
+    _id: "646cc341d602bc6470fcb81a",
+    name: "Typ",
+    type: "Array",
+    arrayOption: [
+      "Kunde",
+      "Liferant"
+    ],
+    objectEntries: []
+  },
+  {
+    _id: "645e1044d4314d4a44b7b805",
+    name: "Frima",
+    type: "String",
+    arrayOption: [],
+    objectEntries: [],
+    index: 0
+  },
+  {
+    _id: "6461eff8c66ef2415c818e0f",
+    name: "Straße",
+    type: "String",
+    arrayOption: [],
+    objectEntries: [],
+    index: 1
+  },
+  {
+    _id: "6461f006c66ef2415c818e14",
+    name: "Hausnummer",
+    type: "Number",
+    minRange: 0,
+    maxRange: 1000,
+    arrayOption: [],
+    objectEntries: [],
+    index: 2
+  },
+  {
+    _id: "6461e4e2c66ef2415c818dc5",
+    name: "Mitarbeiter",
+    type: "Object",
+    arrayOption: [],
+    objectEntries: [
+      {
+        name: "name",
+        type: "String"
+      }
+    ],
+    index: 3
+  }
+]
+const keys=ref(response)
+const values=ref({})
+console.log(keys.value);
+
+function storenNewValue(){
+  console.log(values.value);
+}
+</script>
+
+<template>
+  <BaseInput v-for="itemKey in keys" :itemKey="itemKey" :itemValue="values"/>
+  <button @click="storenNewValue">SAVE</button>
+</template>
+
+<style>
+
+</style>
---
+++ src/BaseInput.vue
@@ -0,0 +1,59 @@
+<script setup>
+const props = defineProps(["itemKey","itemValue"]);
+</script>
+
+<template>
+  <div class="fieldWrapper">
+    <span>{{ props.itemKey.name }}</span>
+<!-- If Object, then Recursion -->
+    <BaseInput 
+      v-if="(props.itemKey.type == 'Object')"
+      v-for="child in props.itemKey.objectEntries"
+      :itemKey="child"
+      :itemValue="props.itemValue[props.itemKey.name]={}"
+    />
+    <input 
+        type=text 
+        v-else
+        v-model="props.itemValue[props.itemKey.name]"
+    >
+
+
+<!-- AddOn To Key if Type== Number -->
+  <!-- <span v-if="props.itemValue.type == 'Number'"
+          >{{ props.itemValue.minRange }} - {{ props.itemValue.maxRange }}</span
+        > -->
+
+<!-- For Input Type String or Date -->
+    <!-- <input
+      v-if="typeof props.itemValue == 'string' || props.itemValue.type == 'Date'"
+      type="text"
+      class="aThird"
+      v-model="props.itemValue"
+    /> -->
+
+<!-- For Input Type Number -->
+    <!-- <input
+      v-if="props.itemValue.type == 'Number'"
+      type="Number"
+      :min="props.itemValue.minRange"
+      :max="props.itemValue.maxRange"
+      class="aThird"
+      v-model="props.itemValue"
+    /> -->
+<!-- For Input Type Boolean -->
+    <!-- <input
+      v-if="props.itemValue.type == 'Boolean'"
+      type="checkbox"
+      class="aThird"
+      v-model="props.itemValue"
+    /> -->
+    
+<!-- For Input Type Array -->
+    <!-- <select v-if="props.itemValue.type == 'Array'" v-model="props.itemValue">
+      <option v-for="option in props.itemValue.arrayOption" :value="option">
+        {{ option }}
+      </option>
+    </select> -->
+  </div>
+</template>
